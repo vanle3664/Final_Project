5c17b2616974ec812fafa88356857aa725e624ee
vân <01658635754levan@gmail.com>
2021-03-30 16:50:36 +0700

    first commit

---
+++ Programming_lesson_06/TrainingFramework/src/GameStates/GSSetting.cpp
@@ -0,0 +1,92 @@
+#include "GSSetting.h"
+
+extern int screenWidth; //need get on Graphic engine
+extern int screenHeight; //need get on Graphic engine
+
+GSSetting::GSSetting()
+{
+
+}
+
+
+GSSetting::~GSSetting()
+{
+}
+
+
+
+void GSSetting::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_play");
+
+	//BackGround
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_BackGround = std::make_shared<Sprite2D>(model, shader, texture);
+	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
+	m_BackGround->SetSize(screenWidth, screenHeight);
+
+	//back button
+	texture = ResourceManagers::GetInstance()->GetTexture("button_back");
+	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(screenWidth/2, 50);
+	button->SetSize(300, 75);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->ChangeState(StateTypes::STATE_Menu);
+		});
+	m_listButton.push_back(button);
+
+}
+
+void GSSetting::Exit()
+{
+}
+
+
+void GSSetting::Pause()
+{
+
+}
+
+void GSSetting::Resume()
+{
+
+}
+
+
+void GSSetting::HandleEvents()
+{
+
+}
+
+void GSSetting::HandleKeyEvents(int key, bool bIsPressed)
+{
+
+}
+
+void GSSetting::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto it : m_listButton)
+	{
+		(it)->HandleTouchEvents(x, y, bIsPressed);
+		if ((it)->IsHandle()) break;
+	}
+}
+
+void GSSetting::Update(float deltaTime)
+{
+	m_BackGround->Update(deltaTime);
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSSetting::Draw()
+{
+	m_BackGround->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+}
